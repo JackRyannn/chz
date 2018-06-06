7312758a176ac384f2b0b530ca881e09fced6534
renchao03 <renchao03@meituan.com>
2018-06-06 17:16:14 +0800

    调试ueditor

--- myapp/models.py
+++ myapp/models.py
@@ -26,8 +26,8 @@ class Article(models.Model):
     category = models.CharField(u"博客标签", max_length=50, blank=True)  # 博客标签
     pub_date = models.DateTimeField(u"发布日期", auto_now_add=True, editable=True)  # 博客发布日期
     update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
-    content = UEditorField(u"文章正文", height=300, width=1000, default=u'', blank=True, imagePath="uploads/blog/images/",
-                           toolbars='besttome', filePath='uploads/blog/files/')
+    content = UEditorField(u"文章正文", height=300, width=1000, default=u'', blank=True, imagePath="./uploads/blog/images/",
+                           toolbars='full', filePath='./uploads/blog/files/')
     def __unicode__(self):
         return self.title
 
